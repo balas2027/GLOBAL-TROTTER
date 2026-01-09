a879f3a675198a365a46af65a57984a044019a55
Balas3010 <144880914+Balas3010@users.noreply.github.com>
2026-01-09 20:50:15 +0530

    changed http to https mistake

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://global-trotter.vercel.app/api/auth';
+const API_URL = 'https://global-trotter.vercel.app/api/auth';
 
 export const login = async (email, password) => {
   const response = await axios.post(`${API_URL}/login`, { email, password });
--- frontend/src/services/itinerary.service.js
+++ frontend/src/services/itinerary.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://global-trotter.vercel.app/api/itineraries';
+const API_URL = 'https://global-trotter.vercel.app/api/itineraries';
 
 const authHeader = () => {
   const token = localStorage.getItem('token');
